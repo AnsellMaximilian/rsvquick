a6efdb5b64890dd566bee7372d0485adf6d5e3f8
Ansell Maximilian <ansellmaximilian@gmail.com>
2023-07-13 06:19:35 +0700

    Add title link to request list card

--- src/components/request-card/list-card.tsx
+++ src/components/request-card/list-card.tsx
@@ -9,6 +9,7 @@ import ValueDisplay from "../common/valueDisplay";
 import { Link } from "react-router-dom";
 import { DeleteButton, EditButton, ShowButton } from "@refinedev/mui";
 import RsvpIcon from "@mui/icons-material/Rsvp";
+import MuiLink from "@mui/material/Link";
 
 interface Props {
   request: IRequest;
@@ -18,9 +19,16 @@ export default function RequestListCard({ request }: Props) {
   return (
     <Card elevation={3} sx={{ flex: 1, minWidth: 250 }}>
       <Box padding={2} bgcolor={request.secondary_color}>
-        <Typography color={request.primary_color} fontWeight="bold">
-          {request.title}
-        </Typography>
+        <MuiLink
+          component={Link}
+          to={`/requests/show/${request.id}`}
+          underline="hover"
+          sx={{ textDecorationColor: `${request.primary_color} !important` }}
+        >
+          <Typography color={request.primary_color} fontWeight="bold">
+            {request.title}
+          </Typography>
+        </MuiLink>
       </Box>
       <Box padding={2}>
         <Stack>
