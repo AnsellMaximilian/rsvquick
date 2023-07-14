ad1179b6f0be7c7cbd6e7f4441063e17b414cf28
Ansell Maximilian <ansellmaximilian@gmail.com>
2023-07-14 15:07:50 +0700

    Add logo

--- src/components/app-icon/index.tsx
+++ src/components/app-icon/index.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Box from "@mui/material/Box";
 
 export const AppIcon: React.FC<{ collapsed?: boolean }> = ({
   collapsed = false,
 }) => {
-  return collapsed ? <h1>R</h1> : <h1>RSVQuick</h1>;
+  return (
+    <Box
+      display="flex"
+      alignItems="center"
+      justifyContent="flex-start"
+      component={Link}
+      to="/requests"
+    >
+      <img
+        src={collapsed ? "/rsvquick-icon.svg" : "/rsvquick-logo.svg"}
+        width={collapsed ? 25 : 120}
+      />
+    </Box>
+  );
 };
